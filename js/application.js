1e0f29dda8ad60d33e8774fa2a057099ab4cf363
“Ian <ianyhkwok@hotmail.com>
2015-10-09 03:58:51 +0800

    Updated

--- js/application.js
+++ js/application.js
@@ -1,7 +1,9 @@
 $(function(){
   var game = new Game();
   var score = 0;
-  console.log(game.rand(100,0));
+  // console.log(game.rand(100,0));
+  var randNum = parseInt(game.rand(100,0));
+  console.log(randNum);
 
 // Set Timer Starts
   var timer;
@@ -24,24 +26,27 @@ $(function(){
   var addTime = function() {
     secondsLeft += 10;
   };
-
-  $('#solution-input').on('click', startTimer);
 // Set Timer End
 
 // Listener to check answer Starts
-    var answerCheck = function () {
+  var answerCheck = function () {
     var x = $('#solution-input').val();
       if (x == 12+8) {
         $('#solution-input').val('');
         addTime();
         console.log("Yeah");
-      } else {
+        var html = ''
+        html = "<p id=\"equation\" class=\"text-center\"> 1000+8 </p>";
+        $('#equation').replaceWith(html);
+        console.log(html)
+        score = score + 5;
+      } else if (x !== 12+8) {
         $('#solution-input').css('border-color', 'red');
         console.log("Wrong");
       };
   }
-  $('#solution-input').keyup(answerCheck);
 // Listener to check answer End
-  // $('#solution-input').validator();
 
+ $('#solution-input').on('click', startTimer);
+ $('#solution-input').keyup(answerCheck);
 });
